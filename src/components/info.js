6a2cfde491e0914206e0bf253dac8ce74ebb1907
manov <imanovplus@gmail.com>
2020-02-08 14:45:38 +0300

    first commit

---
+++ src/components/info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Info extends React.Component {
+  render() {
+    return (
+      <div>
+      <h2>To do list</h2>
+      <p>Добавляй дела в лист, отмечай завершенные!</p>
+      </div>
+    );
+  }
+}
+export default Info;
